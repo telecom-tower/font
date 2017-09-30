e0af9141615d95d8c1731dc08a19a9b81d39ef27
Jacques Supcik <jacques.supcik@hefr.ch>
2017-09-30 16:55:37 +0200

    Initial import from gitlab

---
+++ README.md
@@ -0,0 +1,1 @@
+# Bitmap font for the Telecom Tower
---
+++ font.go
@@ -0,0 +1,59 @@
+// Copyright 2017 Jacques Supcik, Blue Masters
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package font implements bitmap fonts for the Telecom Tower or other 8xN
+// led matrix devices.
+package font
+
+import (
+	"bytes"
+)
+
+var alias = map[rune]string{
+	0x2764:     "\u2665", // ❤
+	0x0001f499: "\u2665", // 💙
+	0x0001f49a: "\u2665", // 💚
+	0x0001f49b: "\u2665", // 💛
+	0x0001f49c: "\u2665", // 💜
+	0x0001f49d: "\u2665", // 💝
+	0x0001F601: ":|",     // 😁
+	0x0001F602: ":)",     // 😂
+	0x0001F603: ":D",     // 😃
+}
+
+// Font is the base type for fonts.
+type Font struct {
+	Width  int             `json:"width"`
+	Height int             `json:"height"`
+	Bitmap map[rune][]byte `json:"bitmap"`
+}
+
+// ExpandAlias replaces special characters in the text (such as smileys)
+// by its ASCII equivalent.
+func ExpandAlias(text string) string {
+	var f func(b *bytes.Buffer, s string)
+	f = func(b *bytes.Buffer, s string) {
+		for _, c := range s {
+			m, ok := alias[c]
+			if ok {
+				f(b, m)
+			} else {
+				b.WriteRune(c)
+			}
+		}
+	}
+	b := new(bytes.Buffer)
+	f(b, text)
+	return b.String()
+}
